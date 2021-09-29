2616f110d8706591ef651843367d9b519b70a786
Vinco Wu <vwu13@outlook.com>
2021-09-29 00:25:26 -0400

    animation adding

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -20,21 +20,21 @@ const Hero = () => {
                     <h1 className="hero-bot__message">Hello!</h1>
                     <h2 className="hero-bot__description">My name is Vinco and welcome to my website! I'm a Front End Web Developer that likes creating websites and applications during my free time! You can learn more about me and my journey <Link className="link" to="/about">here</Link> </h2>
                 </div>
-                <div data-aos="fade-right" className="skills">
+                <div className="skills">
                     <div className="skills-top">
-                        <h1 className="skills-header">My Skills</h1>
-                        <p className="skills-background">
+                        <h1 data-aos="fade-right" className="skills-header">My Skills</h1>
+                        <p data-aos="fade-right" className="skills-background">
                             Through some computer science classes I took during my undergrad, as well as a 12 week Full Stack Web Development bootcamp I completed at BrainStation, I was able to learn new frameworks, languages and technologies. As a junior developer, my goal is to learn something new everyday. Below are some technologies that I've learned so far!
                         </p>
                     </div>
-                    <div data-aos="fade-right" className="skills-bot">
-                        <ul className="skills-list">
+                    <div className="skills-bot">
+                        <ul data-aos="fade-right" className="skills-list">
                             <h2 className="skills-list__type">Languages</h2>
                             {languages.map((language, index) => {
                                 return <li className="skills-list__item" key={'mykey' + index}>{language}</li>
                             })}
                         </ul>
-                        <ul className="skills-list">
+                        <ul data-aos="fade-right" className="skills-list">
                             <h2 className="skills-list__type">Technology</h2>
                             {tech.map((thing, index) => {
                                 return <li className="skills-list__item" key={'mykey' + index}>{thing}</li>
